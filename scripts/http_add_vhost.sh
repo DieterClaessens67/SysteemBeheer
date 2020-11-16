75d9ab56034618f3d594aa6ad0d3abc0114a1f67
DieterClaessens67 <dieterclaessens3@hotmail.com>
2020-11-16 21:56:49 +0100

    added vhost script

---
+++ scripts/http_add_vhost.sh
@@ -0,0 +1,13 @@
+if [ "$(id -u)" -ne 0 ]; then
+        echo "This script must be run by root" >&2
+        exit 1
+fi
+
+FILE = "/etc/apache2/sites-available/mrt/"$VHOST".conf"
+echo "<VirtualHost *:80>" >> $FILE
+echo "ServerName" $VHOST"."$SUBDOMAIN".sb.uclllabs.be" >> $FILE
+echo "ServerAdmin webmaster@localhost" >> $FILE
+echo "DocumentRoot /var/www/"$VHOST"/html" >> $FILE
+echo "ErrorLog ${APACHE_LOG_DIR}/error.log" >> $FILE
+echo "CustomLog ${APACHE_LOG_DIR}/access.log combined" >> $FILE
+echo "</VirtualHost>" >> $FILE
