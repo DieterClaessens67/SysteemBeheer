8b9b530ae629cefd994240ec02ed83801b827245
DieterClaessens67 <dieterclaessens3@hotmail.com>
2020-11-17 21:27:13 +0100

    modified scripts/http_add_vhost.sh

--- scripts/http_add_vhost.sh
+++ scripts/http_add_vhost.sh
@@ -10,22 +10,22 @@ BESTANDSNAAM="db.$ZONE"
 #check of het mrt wel bestaat
 
 if [ -e "/etc/bind/mrt/$BESTANDSNAAM"]; then
-	echo "Er bestaat geen overeenkomstig bestand  in /etc/bind/mrt/..." >&2
+	#.conf bestand aanmaken in /etc/apache2/sites-available/mrt/...
+	touch /etc/apache2/sites-available/mrt/$ZONE.conf
+	FILE="/etc/apache2/sites-available/mrt/"$ZONE".conf"
+	echo "<VirtualHost *:80>" >> $FILE
+	echo "ServerName"$SUB"."$ZONE".sb.uclllabs.be" >> $FILE
+	echo "ServerAdmin webmaster@localhost" >> $FILE
+	echo "DocumentRoot /var/www/"$SUB"/html" >> $FILE
+	echo "ErrorLog ${APACHE_LOG_DIR}/error.log" >> $FILE
+	echo "CustomLog ${APACHE_LOG_DIR}/access.log combined" >> $FILE
+	echo "</VirtualHost>" >> $FILE
+
+	#index.html aanmaken
+	mkdir /var/www/$SUB/html
+	touch /var/www/$SUB/html/index.html
+	FILE2="/var/www/"$SUB"/html/index.html"
+	echo "welcome $SUB.$ZONE" >> $FILE2
+else
 	exit 1
 fi
-
-#.conf bestand aanmaken in /etc/apache2/sites-available/mrt/...
-
-FILE="/etc/apache2/sites-available/mrt/"$ZONE".conf"
-echo "<VirtualHost *:80>" >> $FILE
-echo "ServerName"$SUB"."$ZONE".sb.uclllabs.be" >> $FILE
-echo "ServerAdmin webmaster@localhost" >> $FILE
-echo "DocumentRoot /var/www/"$SUB"/html" >> $FILE
-echo "ErrorLog ${APACHE_LOG_DIR}/error.log" >> $FILE
-echo "CustomLog ${APACHE_LOG_DIR}/access.log combined" >> $FILE
-echo "</VirtualHost>" >> $FILE
-
-#index.html aanmaken
-
-FILE2="/var/www/"$SUB"/html/index.html"
-echo "welcome $SUB.$ZONE" >> $FILE2
