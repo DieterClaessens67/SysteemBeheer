b43ffd933a1618e7aea09d027a73aacf41340a92
DieterClaessens67 <dieterclaessens3@hotmail.com>
2020-11-20 00:59:29 +0100

    proberen http te fixen mess

--- scripts/http_add_vhost.sh
+++ scripts/http_add_vhost.sh
@@ -10,16 +10,16 @@ BESTANDSNAAM="db.$SUB"
 #check of het mrt wel bestaat
 
 if [ -e "/etc/bind/mrt-tests/$BESTANDSNAAM" ]; then
-	#.conf bestand aanmaken in /etc/apache2/sites-available/mrt/...
+	#.conf bestand aanmaken in /etc/apache2/sites-available
 	echo "Maken van de bestanden"
-	touch "/etc/apache2/sites-available/mrt/$ZONE.conf"
-	FILE="/etc/apache2/sites-available/mrt/"$ZONE".conf"
+	touch "/etc/apache2/sites-available/script$ZONE.conf"
+	FILE="/etc/apache2/sites-available/script$ZONE.conf"
 	echo "<VirtualHost *:80>" >> $FILE
-	echo "ServerName"$SUB"."$ZONE".sb.uclllabs.be" >> $FILE
-	echo "ServerAdmin webmaster@localhost" >> $FILE
+	echo "ServerName $ZONE.$SUB.dieter-claessens.sb.uclllabs.be" >> $FILE
+	echo "ServerAdmin root@dieter-claessens.sb.uclllabs.be" >> $FILE
 	echo "DocumentRoot /var/www/$ZONE/$SUB/html" >> $FILE
-	echo "ErrorLog ${APACHE_LOG_DIR}/error.log" >> $FILE
-	echo "CustomLog ${APACHE_LOG_DIR}/access.log combined" >> $FILE
+	echo "ErrorLog \${APACHE_LOG_DIR}/$ZONE-$SUB-error.log" >> $FILE
+	echo "CustomLog \${APACHE_LOG_DIR}/$ZONE-$SUB-access.log combined" >> $FILE
 	echo "</VirtualHost>" >> $FILE
 
 	#index.html aanmaken
@@ -30,7 +30,7 @@ if [ -e "/etc/bind/mrt-tests/$BESTANDSNAAM" ]; then
 	FILE2="/var/www/$ZONE/$SUB/html/index.html"
 	echo "welcome $ZONE.$SUB" >> $FILE2
 
-	a2ensite "mrt/$ZONE.conf"
+	a2ensite script$ZONE.conf
 	systemctl reload apache2
 else
 	echo "Het juiste bestand werd niet gevonden in /etc/bind/mrt-tests/..."
